efcbb1a7213963f5d1c87732cc73da9dfe0cff4c
Chaoyi Chen <chaoyi@walmart.com>
2017-03-10 16:01:54 -0800

    Add magellan integration

--- test/test.js
+++ test/test.js
@@ -1,25 +1,105 @@
 var Config = require('config');
 var Cyancat = require('cyancat');
 var Pages = require('../lib').Pages
+var SauceLabs = require('saucelabs');
+var Moment = require('moment');
 
-var driver = new Cyancat(Config.server, {
-  maxWaitTime: 10000,
+var sauce_username = process.env.SAUCE_USERNAME;
+var sauce_password = process.env.SAUCE_ACCESS_KEY;
+var sauceAccount = new SauceLabs({
+  username: sauce_username,
+  password: sauce_password,
   proxy: Config.proxy
 });
 
-beforeEach(function(done) {
-  driver.run(function() {
-    driver.init(Config.capabilities);
-    done();
-  });
-});
+var driver;
 
-afterEach(function(done) {
-  driver.run(function() {
-    driver.quit();
-    done();
+if (process.env.SAUCE === "true") {
+  // update SauceLabs settings
+  Config.server = "http://" +
+    sauce_username + ":" + sauce_password + "@ondemand.saucelabs.com:80/wd/hub";
+  Config.capabilities.app = "sauce-storage:" + Config.remoteAppName;
+
+  driver = new Cyancat(Config.server, {
+    maxWaitTime: 10000,
+    proxy: Config.proxy
   });
-});
+
+  beforeEach(function(done) {
+    var title = this.currentTest.fullTitle();
+    if (Config.desiredCapabilities) {
+      // Based on --browsers value, Magellan sends back desiredCapabilities to each test
+      // in NODE_CONFIG env variable, which can be extracted in Config.desiredCapabilities
+      // Currently Magellan only returns desiredCapabilities like follows:
+      // {
+      //  "browserName":"iphone",
+      //  "version":"9.2",
+      //  "platform":"OS X 10.10",
+      //  "deviceName":"iPhone Simulator"
+      // }
+      // Pending for https://github.com/TestArmada/guacamole/issues/15 to fully integrate
+      Config.capabilities.platformVersion = Config.desiredCapabilities.version;
+      Config.capabilities.deviceName = Config.desiredCapabilities.deviceName;
+    }
+
+    driver.run(function() {
+      driver.init(Config.capabilities);
+      // feed back selenium session ID to parent process (Magellan)
+      if (process.send) {
+        process.send({
+          type: "selenium-session-info",
+          sessionId: driver.sessionId
+        });
+      }
+
+      // update Sauce dashboard status - job name
+      sauceAccount.updateJob(driver.sessionId, {
+        name: title
+      }, function(err) {
+        if (err) console.log(err);
+      });
+      done();
+    });
+  });
+
+  afterEach(function(done) {
+    var passed = this.currentTest.state === 'passed' ? 1 : 0;
+    driver.run(function() {
+      // update Sauce dashboard status - job result
+      var build = process.env.BUILD === undefined ?
+        Config.remoteAppName + '-' + Moment().format() :
+        Config.remoteAppName + '-' + process.env.BUILD;
+      sauceAccount.updateJob(driver.sessionId, {
+        build: build,
+        passed: passed
+      }, function(err) {
+        if (err) console.log(err);
+      });
+      driver.quit();
+      done();
+    });
+  });
+
+} else {
+  driver = new Cyancat(Config.server, {
+    maxWaitTime: 10000,
+    proxy: Config.proxy
+  });
+
+  beforeEach(function(done) {
+    driver.run(function() {
+      driver.init(Config.capabilities);
+      done();
+    });
+  });
+
+  afterEach(function(done) {
+    driver.run(function() {
+      driver.quit();
+      done();
+    });
+  });
+}
 
 exports.driver = driver;
 exports.pages = Pages;
--- test/wp/publish.js
+++ test/wp/publish.js
@@ -15,40 +15,22 @@ describe('Publish new post user flow', function() {
     });
   });
 
-  it('Publish a new post with valid title, text content and an image gallery of two jpg images in it [C001] @smoke', function(done) {
+  it('Publish a new post with valid title and text content [C001] @smoke', function(done) {
     driver.run(function() {
       test.pages.mysite.createNewPost(driver);
       test.pages.editor.editNewPost(driver);
-      test.pages.editor.createGallery(driver, true);
-      test.pages.editor.publishPost(driver);
-      driver.sleep(2000);
+      //driver.sleep(2000);
       done();
     });
   });
 
-/*
-  it('Update gallery before publish [C002] @smoke', function(done) {
-    driver.run(function() {
-      test.pages.mysite.createNewPost(driver);
-      test.pages.editor.editNewPost(driver);
-      test.pages.editor.createAndUpdateGallery(driver, true);
-      test.pages.editor.publishPost(driver);
-      driver.sleep(5000);
-      done();
-    });
-  });
 
-  it('Publish a new post with multiple image galleries [C003] @smoke', function(done) {
+  it('Publish a new post with valid title and text content [C002] @smoke', function(done) {
     driver.run(function() {
       test.pages.mysite.createNewPost(driver);
       test.pages.editor.editNewPost(driver);
-      test.pages.editor.createGallery(driver, true);
-      test.pages.editor.createGallery(driver);
-      test.pages.editor.publishPost(driver);
-      driver.sleep(2000);
       done();
     });
   });
-*/
 
 });
